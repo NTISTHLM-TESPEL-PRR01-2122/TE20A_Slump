aeeeb95607de25d972819acb724fcbe03f4bf459
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2021-10-19 14:15:06 +0200

    InitialCommit

---
+++ TE20A_Slump/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+Random generator = new Random();
+
+int woodInForest = 1000;
+
+int howMuchWood = generator.Next(100);
+
+Console.WriteLine(howMuchWood);
+
+woodInForest -= howMuchWood;
+
+if (howMuchWood < 50)
+{
+  Console.WriteLine("Not enough wood!");
+}
+else
+{
+  Console.WriteLine("A whole lotta wood!");
+}
+
+Console.ReadLine();
